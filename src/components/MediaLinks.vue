60c2bf8ea45f0b788cec1596943d328292e1dac2
Tim Finley <tfinley@pop-os.localdomain>
2025-03-05 19:58:50 -0500

    added media

---
+++ src/components/MediaLinks.vue
@@ -0,0 +1,9 @@
+<template>
+  <ul>
+    <li><a href="/yardsign_noai.png" download>the yard sign</a></li>
+    <li><a href="/bumper-sticker_noai.png" download>the bumper sticker</a></li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <main>
     <IntoSplash />
+    <MediaLinks />
   </main>
 </template>
 
 <script setup lang="ts">
 import IntoSplash from '../components/IntoSplash.vue'
+import MediaLinks from '../components/MediaLinks.vue'
 </script>
