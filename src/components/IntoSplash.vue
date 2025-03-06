26bf73974a3898a38930df3a30b0d3c1811d031a
Tim Finley <tfinley@pop-os.localdomain>
2025-03-05 20:52:34 -0500

    tweaks

--- src/components/IntoSplash.vue
+++ src/components/IntoSplash.vue
@@ -13,36 +13,5 @@
 </script>
 
 <style>
-.logo {
-  width: 50%;
-}
-.banner-wrap {
-  display: flex;
-  position: relative;
-}
-
-.banner {
-  font-size: 2.3rem;
-  line-height: 0.8;
-  text-align: right;
-  position: absolute;
-  margin-top: 100px;
-}
-
-@media (min-width: 1024px) {
-  .banner h5, h1, h2 {
-    text-shadow: 4px 4px 5px black;
-  }
-  .banner h5 {
-    line-height: 2.5;
-  }
-  .banner h1, h2 {
-    font-size: 10vw;
-    line-height: 0.6;
-  }
-  .logo {
-    right: 150px;
-  }
-}
 
 </style>
--- src/components/MediaLinks.vue
+++ src/components/MediaLinks.vue
@@ -1,9 +1,12 @@
 <template>
-  <ul>
-    <li><a href="/yardsign_noai.png" download>the yard sign</a></li>
-    <li><a href="/bumper-sticker_noai.png" download>the bumper sticker</a></li>
-  </ul>
+  <div class="media-links">
+    <ul>
+      <li><a href="/yardsign_noai.png" download>the yard sign</a></li>
+      <li><a href="/bumper-sticker_noai.png" download>the bumper sticker</a></li>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
+
 </script>
