0a0d7f7524c2600fbd318fc1d4e26e49f4997a2a
Tim Finley <tfinley@pop-os.localdomain>
2025-03-05 21:05:03 -0500

    added link to repo

--- src/components/MediaLinks.vue
+++ src/components/MediaLinks.vue
@@ -3,6 +3,8 @@
     <ul>
       <li><a href="/yardsign_noai.png" download>the yard sign</a></li>
       <li><a href="/bumper-sticker_noai.png" download>the bumper sticker</a></li>
+      <li><a target="_blank" href="https://github.com/maxtimbo/buddycoward" rel="noopener
+          noreferrer">repo for this site</a></li>
     </ul>
   </div>
 </template>
