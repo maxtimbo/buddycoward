5e17eddcf5f25e754451e1331fef3b4b40d7cc4a
Tim Finley <tfinley@pop-os.localdomain>
2025-03-07 21:20:27 -0500

    fixed refresh

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, nextTick } from 'vue';
+import { ref, onMounted, onUnmounted, nextTick, watch } from 'vue';
 import IntoSplash from '../components/IntoSplash.vue';
 import TextBlock from '../components/TextBlock.vue';
 import MediaLinks from '../components/MediaLinks.vue';
@@ -20,12 +20,20 @@ const bannerHeight = ref(0);
 const updateBannerHeight = () => {
   if (bannerComponent.value?.bannerElement) {
     bannerHeight.value = bannerComponent.value.bannerElement.offsetHeight + 10;
-    console.log(bannerHeight.value)
+    console.log(bannerHeight.value);
   }
 };
 
+watch(bannerComponent, (newVal) => {
+  if (newVal) {
+    nextTick(updateBannerHeight);
+  }
+});
+
 onMounted(() => {
-  nextTick(updateBannerHeight);
+  nextTick(() => {
+    setTimeout(updateBannerHeight, 100);
+  });
   window.addEventListener('resize', updateBannerHeight);
 });
 
